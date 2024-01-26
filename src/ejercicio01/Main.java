46aa6bd2effada2daa0dcfcf1e7ffdfa16756338
nestorss05 <nestor.sanchez@iesnervion.es>
2024-01-26 14:21:52 +0100

    Ejercicio 1 FINAL

--- src/ejercicio01/Main.java
+++ src/ejercicio01/Main.java
@@ -1,10 +1,27 @@
 package ejercicio01;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		// num[][]: array bidimensional que guardara numeros
+		int num[][] = new int[3][6];
 		
+		// Asigna los valores pedidos a los respectivos indices
+		num[0][0] = 0;
+		num[0][1] = 30;
+		num[0][2] = 2;
+		num[0][5] = 5;
+		num[1][0] = 75;
+		num[1][4] = 0;
+		num[2][2] = -2;
+		num[2][3] = 9;
+		num[2][5] = 11;
+		
+		// Muestra el array por pantalla
+		System.out.print(Arrays.deepToString(num));
 		
 	}
 
